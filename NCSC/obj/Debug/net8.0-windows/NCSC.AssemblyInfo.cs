52ef4a88856f89949c54443ddcf312de51ff8e9c
Fzkn4 <tomogsokin@gmail.com>
2025-07-22 11:43:03 +0800

    UI minor fixes

--- NCSC/obj/Debug/net8.0-windows/NCSC.AssemblyInfo.cs
+++ NCSC/obj/Debug/net8.0-windows/NCSC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283f7bd28d88fe51262a42e345969f1086103f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35240f8618dd9692b21cedcb6e24bdcf519192b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
