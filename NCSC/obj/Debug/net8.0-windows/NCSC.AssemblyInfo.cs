eb1c41dc5cd9f25e2ed34f84b5f10879d82b6419
Fzkn4 <tomogsokin@gmail.com>
2025-07-30 12:31:21 +0800

    Latest build

--- NCSC/obj/Debug/net8.0-windows/NCSC.AssemblyInfo.cs
+++ NCSC/obj/Debug/net8.0-windows/NCSC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713fa6b7e2644cc55d89695f3902729524e5c09e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d809df9370de3e2bb84cc7c016c2d4d258d46d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NCSC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
